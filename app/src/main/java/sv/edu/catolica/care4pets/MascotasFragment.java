2dee1eb850eaad7b1e8c22400a86281dececd3d8
Jonathan Vanegas <srquike@outlook.com>
2021-11-07 20:40:59 -0600

    Se creo el fragment para agregar nueva mascota

--- app/src/main/java/sv/edu/catolica/care4pets/MascotasFragment.java
+++ app/src/main/java/sv/edu/catolica/care4pets/MascotasFragment.java
@@ -1,6 +1,8 @@
 package sv.edu.catolica.care4pets;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
@@ -9,6 +11,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import java.util.ArrayList;
 
@@ -19,31 +25,18 @@ import java.util.ArrayList;
  */
 public class MascotasFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
     private RecyclerView recyclerView;
     private ArrayList<MascotaViewHolder> lstMascotas;
+    private NavController navController;
 
     public MascotasFragment() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MascotasFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MascotasFragment newInstance(String param1, String param2) {
         MascotasFragment fragment = new MascotasFragment();
         Bundle args = new Bundle();
@@ -75,8 +68,7 @@ public class MascotasFragment extends Fragment {
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Snackbar.make(v, "Se hizo click en el boton nueva mascota!", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                navController.navigate(R.id.nuevaMascotaFragment);
             }
         });
 
@@ -88,6 +80,13 @@ public class MascotasFragment extends Fragment {
 
         recyclerView.setAdapter(adapter);
         return vistaMascotas;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
     }
 
     private void LlenarLista() {
---
+++ app/src/main/java/sv/edu/catolica/care4pets/NuevaMascotaFragment.java
@@ -0,0 +1,96 @@
+package sv.edu.catolica.care4pets;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.util.Calendar;
+
+public class NuevaMascotaFragment extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    private String mParam1;
+    private String mParam2;
+    private EditText edtFechaEsterilizacion;
+    private EditText edtFechaNacimiento;
+    private ImageView imvFechaNacimiento, imvFechaEsterilizacion;
+    private Calendar calendar;
+    private DatePickerDialog datePickerDialog;
+    private String fecha;
+
+    public NuevaMascotaFragment() {
+
+    }
+
+    public static NuevaMascotaFragment newInstance(String param1, String param2) {
+        NuevaMascotaFragment fragment = new NuevaMascotaFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View vista = inflater.inflate(R.layout.fragment_nueva_mascota, container, false);
+
+        edtFechaNacimiento = vista.findViewById(R.id.edtFechaNacimiento);
+        edtFechaEsterilizacion = vista.findViewById(R.id.edtFechaEsterilizacion);
+        imvFechaNacimiento = vista.findViewById(R.id.imVFechaNacimiento);
+        imvFechaEsterilizacion = vista.findViewById(R.id.imVFechaEsterilizacion);
+
+        imvFechaNacimiento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirCalendario(edtFechaNacimiento);
+            }
+        });
+
+        imvFechaEsterilizacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirCalendario(edtFechaEsterilizacion);
+            }
+        });
+
+        return vista;
+    }
+
+    private void abrirCalendario(EditText editText) {
+        calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int mes = calendar.get(Calendar.MONTH);
+        int dia = calendar.get(Calendar.DAY_OF_MONTH);
+
+        datePickerDialog = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                if (editText.equals(edtFechaNacimiento)) {
+                    edtFechaNacimiento.setText(dayOfMonth + "/" + month + "/" + year);
+                } else if (editText.equals((edtFechaEsterilizacion))){
+                    edtFechaEsterilizacion.setText(dayOfMonth + "/" + month + "/" + year);
+                }
+            }
+        }, year, mes, dia);
+
+        datePickerDialog.show();
+    }
+}
