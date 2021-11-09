e455e15bde834e46a4330b52d1ceffcbc29cfd92
Jonathan Vanegas <srquike@outlook.com>
2021-11-08 18:47:51 -0600

    Se creo el fragment para la agenda

--- app/src/main/java/sv/edu/catolica/care4pets/NuevaMascotaFragment.java
+++ app/src/main/java/sv/edu/catolica/care4pets/NuevaMascotaFragment.java
@@ -4,6 +4,8 @@ import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
@@ -40,10 +42,17 @@ public class NuevaMascotaFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.save_or_cancel_menu, menu);
     }
 
     @Override
