63ddc2fa2743eaab3171520b2bcb32c0b44a40c2
Obito <douglas.uod@gmail.com>
2021-11-10 15:43:30 -0600

    rellenar datos en nueva mascota

--- app/src/main/java/sv/edu/catolica/care4pets/ControladorBD.java
+++ app/src/main/java/sv/edu/catolica/care4pets/ControladorBD.java
@@ -16,9 +16,9 @@ public class ControladorBD extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create table Roles(ID_Rol integer primary key, Nombre text)");
         db.execSQL("CREATE TABLE Usuario(ID_User integer primary key, Nombre text, Apellido text, correo text, contraseña text)");
-        db.execSQL("CREATE TABLE Mascotas(ID_pet integer primary key,Nombre text, Apellido text, Raza text,Sexo text,Especie text, Color text,FechaNaci date,Esterilizacion text,FechaEsterilizacion date)");
-        db.execSQL("CREATE TABLE Evento(ID_Evento integer primary key,Nombre text, Fecha date,Hora time,Mascota text,TipoEvento text,Descripcion text)");
-        db.execSQL("CREATE TABLE Medicamentos(ID_Medicamento integer primary key,Nombre text,dosis integer,hora_ini time,notas text)");
+        db.execSQL("CREATE TABLE Mascotas(ID_pet integer primary key,Nombre text, Apellido text, Raza text,Sexo text,Especie text, Color text,FechaNaci date,Esterilizacion boolean,FechaEsterilizacion date)");
+        db.execSQL("CREATE TABLE Evento(ID_Evento integer primary key,Nombre text, Fecha date,Hora datetime,Mascota text,TipoEvento text,Descripcion text)");
+        db.execSQL("CREATE TABLE Medicamentos(ID_Medicamento integer primary key,Nombre text,dosis integer,hora_ini datetime,notas text)");
         db.execSQL("CREATE TABLE Alimentos(ID_comida integer primary key,Nombre text, cantidad integer,fecha_ini date,Tomas integer,Notas text)");
         db.execSQL("CREATE TABLE Recordatorio(integer primary key,Nombre text,Fecha date,notas text) ");
         db.execSQL("CREATE TABLE Profesionales(ID_Profesionales integer primary key,Nombre text,Apellido text,Correo text,Telefono int)");
