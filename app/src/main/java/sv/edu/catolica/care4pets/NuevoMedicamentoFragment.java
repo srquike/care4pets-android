863b8bc5b9d37904fcd279b135352634e7213c19
Obito <douglas.uod@gmail.com>
2021-11-12 17:24:03 -0600

    Base de datos lista Nuevo Medicamento Listo

--- app/src/main/java/sv/edu/catolica/care4pets/NuevoMedicamentoFragment.java
+++ app/src/main/java/sv/edu/catolica/care4pets/NuevoMedicamentoFragment.java
@@ -1,5 +1,7 @@
 package sv.edu.catolica.care4pets;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -7,6 +9,9 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class NuevoMedicamentoFragment extends Fragment {
 
@@ -14,6 +19,14 @@ public class NuevoMedicamentoFragment extends Fragment {
     private static final String ARG_PARAM2 = "param2";
     private String mParam1;
     private String mParam2;
+
+
+    private EditText edtNombre,edtCantidad, edtLaboratorio,edtNotas, edtFechaVencimiento;
+    private Spinner spnPresentacion, spnUnidad;
+
+
+    ControladorBD adminDB;
+    SQLiteDatabase db;
 
     public NuevoMedicamentoFragment() {
 
@@ -37,6 +50,10 @@ public class NuevoMedicamentoFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        adminDB = new ControladorBD(getContext(),"DBCare4Pets",null,1);
+        db = adminDB.getWritableDatabase();
+
     }
 
     @Override
@@ -50,6 +67,52 @@ public class NuevoMedicamentoFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_nuevo_medicamento, container, false);
 
+        edtNombre = view.findViewById(R.id.txtNombre);
+        edtCantidad = view.findViewById(R.id.txtCantidad);
+        edtLaboratorio = view.findViewById(R.id.txtLaboratorio);
+        edtFechaVencimiento = view.findViewById(R.id.edtFechaVencimiento);
+
+
         return view;
     }
+
+    private void insertToDB(){
+
+        ContentValues values = new ContentValues();
+        values.put("ID_Medicamento",1);
+        values.put("Nombre", edtNombre.getText().toString());
+        values.put("Presentacion",spnPresentacion.getSelectedItem().toString());
+        values.put("Cantidad",edtCantidad.getText().toString());
+        values.put("Unidad",spnUnidad.getSelectedItem().toString());
+        values.put("FechaVencimiento",edtFechaVencimiento.getText().toString());
+        values.put("Laboratorio",edtLaboratorio.getText().toString());
+        values.put("notas",edtNotas.getText().toString());
+
+
+
+        long id = db.insert("Medicamentos",null,values);
+        if (id>0){
+            MostrarMensaje("Medicamento Agregado");
+        }else{
+            MostrarMensaje("Error al Ingresar Datos");
+        }
+        LimpiarCasillas();
+        db.close();
+    }
+
+    private void LimpiarCasillas() {
+        edtNombre.setText("");
+        edtCantidad.setText("");
+        edtFechaVencimiento.setText("");
+        edtLaboratorio.setText("");
+        edtNotas.setText("");
+
+
+    }
+
+    private void MostrarMensaje(String msg) {
+        Toast.makeText(getContext(), msg, Toast.LENGTH_SHORT).show();
+    }
+
+
 }
